b9c4e846487a48eedb95ef0fcf3de973ddea4600
kod378 <kod378@naver.com>
2022-04-25 00:33:56 +0900

    1. 메인페이지에 조회 추가

--- src/main/java/org/jojoldu/springboot/service/posts/PostsService.java
+++ src/main/java/org/jojoldu/springboot/service/posts/PostsService.java
@@ -4,11 +4,15 @@ package org.jojoldu.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.jojoldu.springboot.domain.posts.Posts;
 import org.jojoldu.springboot.domain.posts.PostsRepository;
+import org.jojoldu.springboot.web.dto.PostsListResponseDto;
 import org.jojoldu.springboot.web.dto.PostsResponseDto;
 import org.jojoldu.springboot.web.dto.PostsSaveRequestDto;
 import org.jojoldu.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
